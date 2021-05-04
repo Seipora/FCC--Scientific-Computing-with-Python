a3339fe81ebc6a847e59c0d1b81699bb47a0fd5e
Seipora <76661978+Seipora@users.noreply.github.com>
2021-05-04 15:15:11 +0200

    removing some comments

--- ProbabilityCalculator/probability_calculator.py
+++ ProbabilityCalculator/probability_calculator.py
@@ -3,7 +3,7 @@
 # For example: hat1 = Hat(yellow=3, blue=2, green=6); hat3 = Hat(red=5, orange=4, black=1, blue=0, pink=2, striped=9)
 # A hat will always be created with at least one ball.
 
-# import copy
+
 import random
 
 
@@ -95,45 +95,3 @@ def check_balls(a,b):
     for x in final_result:
         if x == "False":
             return False
-
-
-
-# hat = Hat(blue=3,red=2,green=6)
-# print(experiment(hat=hat, expected_balls={"blue":2,"green":1}, num_balls_drawn=4, num_experiments=10))
-
-# hat = Hat(yellow=5,red=1,green=3,blue=9,test=1)
-# hat = Hat(yellow=4,blue=3,test=1)
-# print(experiment(hat=hat, expected_balls={"yellow":1,"blue":1,"test":1}, num_balls_drawn=3, num_experiments=100))
-
-import unittest
-class UnitTests(unittest.TestCase):
-    def test_hat_class_contents(self):
-        hat = Hat(red=3,blue=2)
-        actual = hat.contents
-        expected = ["red","red","red","blue","blue"]
-        self.assertEqual(actual, expected, 'Expected creation of hat object to add correct contents.')
-
-    def test_hat_draw(self):
-        hat = Hat(red=5,blue=2)
-        actual = hat.draw(2)
-        expected = ['blue', 'red']
-        self.assertEqual(actual, expected, 'Expected hat draw to return two random items from hat contents.')
-        actual = len(hat.contents)
-        expected = 5
-        self.assertEqual(actual, expected, 'Expected hat draw to reduce number of items in contents.')
-
-    def test_prob_experiment(self):
-        hat = Hat(blue=3,red=2,green=6)
-        probability = experiment(hat=hat, expected_balls={"blue":2,"green":1}, num_balls_drawn=4, num_experiments=1000)
-        actual = probability
-        expected = 0.272
-        self.assertAlmostEqual(actual, expected, delta = 0.01, msg = 'Expected experiment method to return a different probability.')
-        hat = Hat(yellow=5,red=1,green=3,blue=9,test=1)
-        probability = experiment(hat=hat, expected_balls={"yellow":2,"blue":3,"test":1}, num_balls_drawn=20, num_experiments=100)
-        actual = probability
-        expected = 1.0
-        self.assertAlmostEqual(actual, expected, delta = 0.01, msg = 'Expected experiment method to return a different probability.')
-
-
-if __name__ == "__main__":
-    unittest.main()
